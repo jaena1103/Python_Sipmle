81a35e27a6254e38ac2873ef8aca436eb5571810
jaena1103 <133843598+jaena1103@users.noreply.github.com>
2023-10-06 13:27:57 +0900

    Update_practice_01_for.py

--- practice/practice_01_for.py
+++ practice/practice_01_for.py
@@ -30,5 +30,29 @@ print(round(result, 2))  # 평균값
 
 # 문제4) list b에서 최소값 찾기
 b = [22, 1, 4, 7, 98]
+# num_min = b[0]
+# for num in b:
+#     if num < num_min:
+#         num_min = num
+# print(num_min)  # 1 출력
 
-print(num_min)  # 1 출력
+num_min = b[0] # 22
+for x in b:
+    if x < num_min:
+        num_min = x
+        print(f"최소값: {num_min}")
+# b 안에 있는거 오름차순으로 정렬 해보기
+
+# 문제5) list c의 최소값, 최대값 찾기
+c = [2, 5, 7, 1, 8]
+num_min = c[0]
+num_max = c[0]
+for x in c:
+    if x < num_min:  # 최소값 찾기
+        num_min = x
+    if x > num_max:  # 최대값 찾기
+        num_max = x
+print(f"최소값: {num_min}")
+print(f"최대값: {num_max}")
+
+
