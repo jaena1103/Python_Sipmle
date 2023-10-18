49c7c069174bbf219664603a963556b005f75afc
jaena1103 <133843598+jaena1103@users.noreply.github.com>
2023-10-18 17:10:26 +0900

    service_kiosk.py

--- project_kiosk/service_kiosk.py
+++ project_kiosk/service_kiosk.py
@@ -14,6 +14,12 @@ def user_choice(max_cnt, menu_type=""):
             print("MSG: 올바른 번호를 입력하세요.")
 
 
+# 메뉴 출력 기능
+
+def show_menu(menu_list):
+    for i, menu in enumerate(menu_list.values()):
+        print(f"■□  {i + 1}.{menu}")
+
 
 
 
